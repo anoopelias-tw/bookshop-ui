fcece016183630f5076b964e5327418fa2fb481e
Anoop Elias <Anoop.Elias@thoughtworks.com>
2023-12-18 12:07:15 +0530

    Use react component for showing tokens

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useEffect, useState } from "react";
 import Header from './Header/Header';
 
 function login() {
-  window.location.href = "http://localhost:8090/default/authorize?client_id=debugger&scope=openid+user&response_type=code&response_mode=query&state=1234&nonce=5678&redirect_uri=http%3A%2F%2Flocalhost%3A3000";
+    window.location.href = "http://localhost:8090/default/authorize?client_id=debugger&scope=openid+user&response_type=code&response_mode=query&state=1234&nonce=5678&redirect_uri=http%3A%2F%2Flocalhost%3A3000";
 }
 
-function fetchToken() {
-  const url = new URL(window.location.href);
-  const code = url.searchParams.get("code");
+const Tokens = () => {
+    const [tokens, setTokens] = useState({});
+    useEffect(() => {
+        const url = new URL(window.location.href);
+        const code = url.searchParams.get("code");
 
-  if (!code) {
-    return;
-  }
+        if (!code) {
+            return;
+        }
 
-  const data = {
-    code: code,
-    grant_type: "authorization_code",
-    scope: "openid role",
-    redirect_uri: "http://localhost:3000",
-    client_id: "bookshop",
-  };
+        const data = {
+            code: code,
+            grant_type: "authorization_code",
+            scope: "openid role",
+            redirect_uri: "http://localhost:3000",
+            client_id: "bookshop",
+        };
 
-  var formBody = [];
-  for (var property in data) {
-    var encodedKey = encodeURIComponent(property);
-    var encodedValue = encodeURIComponent(data[property]);
-    formBody.push(encodedKey + "=" + encodedValue);
-  }
+        var formBody = [];
+        for (var property in data) {
+            var encodedKey = encodeURIComponent(property);
+            var encodedValue = encodeURIComponent(data[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
 
-  formBody = formBody.join("&");
-  fetch("http://localhost:8090/default/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "origin": "http://localhost:3000",
-    },
-    body: formBody
-  }).then((response) => {
-    return response.json();
-  }).then ((responseJson) => {
-    console.log(responseJson);
-  });
+        formBody = formBody.join("&");
+        fetch("http://localhost:8090/default/token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "origin": "http://localhost:3000",
+            },
+            body: formBody
+        }).then((response) => {
+            return response.json();
+        }).then ((responseJson) => {
+            setTokens(responseJson);
+        });
+    }, {});
+
+    return <div>Tokens:
+            <p>access_token:{tokens["access_token"]}</p>
+            <p>id_token:{tokens["id_token"]}</p>
+            <p>refresh_token:{tokens["refresh_token"]}</p>
+            </div>;
 }
 
 function App() {
-  fetchToken();
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Tokens />
         <button onClick={login}>Login</button>
       </header>
     </div>
